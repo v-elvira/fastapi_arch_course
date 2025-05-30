91e52b545e423cfb1c54270a29aacd0c3df4a128
elya <vavina.elvira@gmail.com>
2025-05-30 06:20:07 +0300

    alembic.ini: hook black and file_template

---
+++ src/migrations/versions/2025_05_30_0616-369932a279a6_add_hotels.py
@@ -0,0 +1,35 @@
+"""add hotels
+
+Revision ID: 369932a279a6
+Revises:
+Create Date: 2025-05-30 06:16:08.378797
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "369932a279a6"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        "hotels",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("title", sa.String(length=100), nullable=False),
+        sa.Column("location", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table("hotels")
---
+++ src/migrations/versions/2025_05_30_0617-cde5b29ad466_add_rooms.py
@@ -0,0 +1,42 @@
+"""add rooms
+
+Revision ID: cde5b29ad466
+Revises: 369932a279a6
+Create Date: 2025-05-30 06:17:29.942005
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "cde5b29ad466"
+down_revision: Union[str, None] = "369932a279a6"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        "rooms",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("hotel_id", sa.Integer(), nullable=False),
+        sa.Column("title", sa.String(), nullable=False),
+        sa.Column("description", sa.String(), nullable=True),
+        sa.Column("price", sa.Integer(), nullable=False),
+        sa.Column("quantity", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["hotel_id"],
+            ["hotels.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table("rooms")
