4a0701059f00cc1cbb2143756a1265f6320a96f8
elya <vavina.elvira@gmail.com>
2025-07-19 01:16:00 +0300

    add hash to password in /register

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,14 +1,17 @@
 from fastapi import APIRouter
+from passlib.context import CryptContext
 from src.database import async_session_maker
 from src.schemas.users import UserRequestAdd, UserAdd, User
 from src.repositories.users import UsersRepository
 
 router = APIRouter(prefix='/auth', tags=['User auth'])
 
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
 @router.post('/register')
 async def register(user_data: UserRequestAdd) -> dict[str, str | User]:
     new_data = UserRequestAdd.model_dump(user_data)
-    new_data['hashed_password'] = str(user_data.password)
+    new_data['hashed_password'] = pwd_context.hash(user_data.password)
     del new_data['password']
     new_user_data = UserAdd(**new_data)
     async with async_session_maker() as session:
