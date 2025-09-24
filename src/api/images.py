1c228ab3e328150115cf0db77c8927d9ff4ea058
elya <vavina.elvira@gmail.com>
2025-09-24 23:40:41 +0300

    change upload_image: Celery task -> BackgroundTasks

--- src/api/images.py
+++ src/api/images.py
@@ -1,14 +1,23 @@
 import shutil
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, UploadFile, BackgroundTasks
 
 from src.tasks.tasks import resize_image
 
 router = APIRouter(prefix='/images', tags=['Hotel images'])
 
+# @router.post('')
+# def upload_image_celery_version(file: UploadFile):
+#     image_path = f'src/static/images/{file.filename}'  # Uvicorn is started from src parent folder
+#     with open(image_path, 'wb+') as new_file:
+#         shutil.copyfileobj(file.file, new_file)
+#
+#     resize_image.delay(image_path)  # resize_image must be decorated with @celery_instance.task
+
 @router.post('')
-def upload_image(file: UploadFile):
+def upload_image(file: UploadFile, bg_tasks: BackgroundTasks):
     image_path = f'src/static/images/{file.filename}'  # Uvicorn is started from src parent folder
     with open(image_path, 'wb+') as new_file:
         shutil.copyfileobj(file.file, new_file)
 
-    resize_image.delay(image_path)
+    # resize_image.delay(image_path)
+    bg_tasks.add_task(resize_image, image_path)
--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -12,7 +12,7 @@ def test_task():
     time.sleep(5)
     print('My test task done!')
 
-@celery_instance.task
+# @celery_instance.task
 def resize_image(image_path: str):
     sizes = [1000, 500, 200]
     output_folder = 'src/static/images'  # Ok if celery and uvicorn are started from src parent folder
