845f9d10754981fdb2fbf7a8dc5952443d7b5fca
elya <vavina.elvira@gmail.com>
2025-09-25 04:05:40 +0300

    add first pytest tests

---
+++ tests/intergation_tests/test_auth.py
@@ -0,0 +1,12 @@
+from src.services.auth import AuthService
+
+def test_create_and_decode_token():
+    data = {'user_id': 1}
+    jwt_token = AuthService().create_access_token(data)
+
+    assert jwt_token
+    assert isinstance(jwt_token, str)
+
+    payload = AuthService().decode_token(jwt_token)
+    assert payload
+    assert payload.get('user_id') == data['user_id']
