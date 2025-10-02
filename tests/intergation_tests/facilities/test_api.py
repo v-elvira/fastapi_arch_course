c85266f721939517e0daefd910fcac8f48da9f04
elya <vavina.elvira@gmail.com>
2025-10-02 09:57:48 +0300

    add isinstance asserts to facility tests

--- tests/intergation_tests/facilities/test_api.py
+++ tests/intergation_tests/facilities/test_api.py
@@ -1,16 +1,21 @@
 from tests.conftest import client
 
 async def test_create_facility(client):
+    test_title = 'Wi-Fi'
     result = await client.post(
         '/facilities',
-        json={'title': 'Wi-Fi'},
+        json={'title': test_title},
     )
-    print(f'Response create facility: {result.json()}')
+    # print(f'Response create facility: {result.json()}')
     assert result.status_code == 201
-    assert result.json()['data']['title'] == 'Wi-Fi'
+    result_json = result.json()
+    assert isinstance(result_json, dict)
+    assert 'data' in result_json
+    assert result_json['data']['title'] == test_title
 
 
 async def test_get_facilities(client):
     result = await client.get('/facilities')
-    print(f'Rescponse get facilities: {result.json()}')   #  [{'title': 'Wi-Fi', 'id': 1}]
+    # print(f'Rescponse get facilities: {result.json()}')   #  [{'title': 'Wi-Fi', 'id': 1}]
     assert result.status_code == 200
+    assert isinstance(result.json(), list)
