92f65d7ee6f286c97649f6c6551dd6e94d293333
elya <vavina.elvira@gmail.com>
2025-10-03 21:03:41 +0300

    add test_add_booking

--- tests/intergation_tests/bookings/test_api.py
+++ tests/intergation_tests/bookings/test_api.py
@@ -1,5 +1,23 @@
 from tests.conftest import authenticated_client
 
+async def test_add_booking(db, authenticated_client):
+    room_id = (await db.rooms.get_all())[0].id
+    result = await authenticated_client.post(
+        '/bookings',
+        json={
+            'room_id': room_id,
+            'date_from': '2025-10-01',
+            'date_to': '2025-10-02',
+        }
+    )
+    assert result.status_code == 201
+    result_json = result.json()
+    assert isinstance(result_json, dict)
+    assert result_json.get('status') == 'OK'
+    assert 'data' in result_json
+    assert result_json['data'].get('room_id') == room_id
+
+
 async def test_get_my_bookings(authenticated_client):
     result = await authenticated_client.get(
         '/bookings/me',
