d767f05383d247e3cb827babb8ee5446d4c36cff
elya <vavina.elvira@gmail.com>
2025-10-04 15:12:29 +0300

    add @pytest.mark.parametrize to test_add_booking

--- tests/intergation_tests/bookings/test_api.py
+++ tests/intergation_tests/bookings/test_api.py
@@ -1,51 +1,31 @@
+import pytest
 from tests.conftest import authenticated_client
 
-async def test_add_booking(db, authenticated_client):
-    room_id = (await db.rooms.get_all())[0].id
+@pytest.mark.parametrize('room_id, date_from, date_to, status_code', [
+     (1, '2025-10-01', '2025-10-08', 201),
+     (1, '2025-10-03', '2025-10-10', 201),
+     (1, '2025-10-01', '2025-10-09', 201),
+     (1, '2025-10-01', '2025-10-10', 201),
+     pytest.param(1, '2025-10-01', '2025-10-10', 201, id='last free room'),
+     pytest.param(1, '2025-10-01', '2025-10-10', 409, id='no free rooms'),
+     (1, '2025-10-09', '2025-10-12', 201),
+ ])
+async def test_add_booking(room_id, date_from, date_to, status_code, db, authenticated_client):
     result = await authenticated_client.post(
         '/bookings',
         json={
             'room_id': room_id,
-            'date_from': '2025-10-01',
-            'date_to': '2025-10-02',
+            'date_from': date_from,
+            'date_to': date_to,
         }
     )
-    assert result.status_code == 201
+    assert result.status_code == status_code
     result_json = result.json()
     assert isinstance(result_json, dict)
-    assert result_json.get('status') == 'OK'
-    assert 'data' in result_json
-    assert result_json['data'].get('room_id') == room_id
-
-
-async def test_add_booking_extra_temp(db, authenticated_client):
-    room_id = (await db.rooms.get_all())[0].id
-    for i in range(4):
-        result = await authenticated_client.post(
-            '/bookings',
-            json={
-                'room_id': room_id,
-                'date_from': '2025-10-01',
-                'date_to': '2025-10-02',
-            }
-        )
-        assert result.status_code == 201
-        result_json = result.json()
-        assert isinstance(result_json, dict)
+    if status_code == 201:
         assert result_json.get('status') == 'OK'
         assert 'data' in result_json
         assert result_json['data'].get('room_id') == room_id
-
-    result = await authenticated_client.post(
-        '/bookings',
-        json={
-            'room_id': room_id,
-            'date_from': '2025-10-01',
-            'date_to': '2025-10-02',
-        }
-    )
-    assert result.status_code == 409
-    assert isinstance(result.json(), dict)
 
 
 async def test_get_my_bookings(authenticated_client):
