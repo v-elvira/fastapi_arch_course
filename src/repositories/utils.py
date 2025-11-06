6a9125e41459fd3ce1296827e82cd3788db3f3f3
elya <vavina.elvira@gmail.com>
2025-11-06 05:48:43 +0300

    add comment with original booking query version

--- src/repositories/utils.py
+++ src/repositories/utils.py
@@ -27,3 +27,48 @@ def room_ids_for_booking(
     room_ids = select(free_rooms.c.room_id).select_from(free_rooms).filter(free_rooms.c.free_count > 0)
     # print(room_ids.compile(bind=engine, compile_kwargs={"literal_binds": True}))
     return room_ids
+
+
+## ORIGINAL course variant:
+
+# def rooms_ids_for_booking(
+#     date_from: date,
+#     date_to: date,
+#     hotel_id: int | None = None,
+# ) -> Select:
+#     rooms_count = (
+#         select(BookingsOrm.room_id, func.count("*").label("rooms_booked"))
+#         .select_from(BookingsOrm)
+#         .filter(
+#             BookingsOrm.date_from <= date_to,
+#             BookingsOrm.date_to >= date_from,
+#         )
+#         .group_by(BookingsOrm.room_id)
+#         .cte(name="rooms_count")
+#     )
+#
+#     rooms_left_table = (
+#         select(
+#             RoomsOrm.id.label("room_id"),
+#             (RoomsOrm.quantity - func.coalesce(rooms_count.c.rooms_booked, 0)).label("rooms_left"),
+#         )
+#         .select_from(RoomsOrm)
+#         .outerjoin(rooms_count, RoomsOrm.id == rooms_count.c.room_id)
+#         .cte(name="rooms_left_table")
+#     )
+#
+#     rooms_ids_for_hotel = select(RoomsOrm.id).select_from(RoomsOrm)
+#     if hotel_id is not None:
+#         rooms_ids_for_hotel = rooms_ids_for_hotel.filter_by(hotel_id=hotel_id)
+#
+#     rooms_ids_for_hotel_subq: Subquery = rooms_ids_for_hotel.subquery(name="rooms_ids_for_hotel")
+#
+#     rooms_ids_to_get = (
+#         select(rooms_left_table.c.room_id)
+#         .select_from(rooms_left_table)
+#         .filter(
+#             rooms_left_table.c.rooms_left > 0,
+#             rooms_left_table.c.room_id.in_(rooms_ids_for_hotel_subq),  # type: ignore
+#         )
+#     )
+#     return rooms_ids_to_get
